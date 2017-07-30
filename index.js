492543cd7c2ac59d83f71908a3b594fe135ca0fc
Brian Winkers <bwinkers@gmail.com>
2017-07-29 20:37:02 -0700

    Working as standalone module

---
+++ index.js
@@ -0,0 +1,22 @@
+var ar = require('./lib/ar.js');
+
+module.exports = function(options) {
+
+  return function activeRules(req, res, next) {
+    // use options to control handler's behavior
+    
+    ar.getSiteByHost(req.headers.host, options)
+    .then(function (site) {
+        req.ar = ar;
+        console.log('ActiveRules initialized site: ' + site.site);
+        next();
+    })
+    .catch(function (err) {
+        
+        console.log(err.message); 
+        
+        res.status(404) // HTTP status 404: NotFound
+        .send('Domain or host support not found');
+    });
+  }
+};
