7dc5d8c56fff2a99750301e1db2f3596cc54682a
Brian Winkers <bwinkers@gmail.com>
2017-08-17 19:29:35 -0700

    Default to not throwing a 404.
    That is now optional behavior by passing in a `return404=true` in options.

--- index.js
+++ index.js
@@ -5,21 +5,35 @@ var ar = require('./lib/ar-site.js');
 module.exports = function(options) {
 
   return function activeRules(req, res, next) {
-    // use options to control handler's behavior
-    
+
+    // Retrieve the site for this hostname
     ar.getSiteByHost(req.headers.host, options)
     .then(function (site) {
-        req.ar = {};
+        // If the request ActiveRules (ar) object is an object, create it.
+        if(typeof req.ar != 'object') {
+            req.ar = {};
+        }
+        // Set the site data in the request AR object
         req.ar.site = site;
+        
         console.log('ActiveRules initialized site: ' + req.ar.site.site + ' for host: ' + req.headers.host + " from arRoot: " + options);
+        
+        // Call next middleware
         next();
     })
     .catch(function (err) {
         
         console.log(err.message); 
         
-        res.status(404) // HTTP status 404: NotFound
-        .send('Domain or host support not found');
+        // Do we want to throw a 404 if we don't find a site?
+        if(typeof options.throw404 != 'undefined' && options.throw404 != false) {
+            // Return a 404
+            res.status(404) // HTTP status 404: NotFound
+            .send('Domain or host support not found');
+        } else {
+            // Call next middleware
+            next();
+        }
     });
   }
 };
